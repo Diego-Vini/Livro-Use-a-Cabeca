d49a494e1011ad3346b97c3d4003c2e0b8bc6a83
Diego Vini <analistamm@gmail.com>
2021-01-31 18:31:54 -0300

    Sorteio de um sundae

--- cap_1/primeiro_codigo.py
+++ cap_1/primeiro_codigo.py
@@ -1,1 +1,22 @@
 # Iniciando a jornada python
+# Importar a biblioteca random e utilizar variaveis e array para sortear um sundae
+
+import random
+
+clientes = ['Jimmy', 'Kim', 'John', 'Stacie']
+
+vencedor = random.choice(clientes)
+
+sabor = 'baunilha'
+
+print('Parabéns ' + vencedor + ' você ganhou um sundae')
+
+prompt = 'Você quer uma cereja em cima?'
+
+quer_cereja = input(prompt)
+
+pedido = 'sundae de' + sabor
+if (quer_cereja == 'sim'):
+    pedido = pedido + ' com uma cereja em cima'
+
+print('Um ' + pedido + ' para ' + vencedor + ' saindo')
